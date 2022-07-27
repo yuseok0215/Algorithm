8e53b1207100e7a88f83cafe7769dced3922c233
hajihong <hajihong96@gmail.com>
2022-07-27 19:33:22 +0900

    [백준][정렬]

---
+++ Algorithm/하지홍/백준/정렬/11000.py
@@ -0,0 +1,29 @@
+import heapq
+
+n = int(input())
+
+arr = []
+classes = []
+
+for i in range(n):
+    a,b = map(int, input().split())
+    arr.append((a,b))
+    
+arr.sort(key=lambda x : x[0])
+print(arr)
+
+heapq.heappush(classes, arr[0][1])
+
+for i in range(1, n):
+    a, b = arr[i][0], arr[i][1]
+    if a >= classes[0]:
+        heapq.heappop(classes)
+        heapq.heappush(classes,b)
+    else:
+        heapq.heappush(classes, b)
+        
+print(len(classes))
+        
+
+        
+
